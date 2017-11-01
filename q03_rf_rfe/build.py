e84e94e3c28264669de4783d0f4512f76bc53733
shashi07 <shashi.mishra43@gmail.com>
2017-11-01 03:06:05 +0000

    Done

--- q03_rf_rfe/build.py
+++ q03_rf_rfe/build.py
@@ -9,4 +9,16 @@ from sklearn.ensemble import RandomForestClassifier
 
 # Your solution code here
 
+def rf_rfe(df):
+    model = RandomForestClassifier()
+    X, y = df.iloc[:,:-1], df.iloc[:,-1]
+    rfe = RFE(model, df.shape[1]/2)
+    rfe = rfe.fit(X, y)
 
+    t = df.iloc[:,rfe.support_]
+    fs = list(t)
+    feature_names = list(X)
+    #ordered = [feature_names[i] for i in np.argsort(rfe.ranking_)[::-1]]
+    #print fs
+    #print ordered
+    return fs
